ff02ca368b94ad8c946d81f29008e9f0c79033e0
vickyg49 <vikkyganesh7929@gmail.com>
2025-08-25 16:12:18 +0530

    Updated settings.py for deployment (ALLOWED_HOSTS, database, static files)

--- backend/settings.py
+++ backend/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, default="postgresql://postgres:postgres@localhost:5432/taskforge_db")
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
