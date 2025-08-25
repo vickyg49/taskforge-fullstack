cbf4012428f2ff3125fd2849ed62d76a3fc26436
vickyg49 <vikkyganesh7929@gmail.com>
2025-08-26 00:03:40 +0530

    urls test

--- backend/urls.py
+++ backend/urls.py
@@ -1,14 +1,24 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from tasks.views import TaskViewSet
-from .views import index
+# from django.contrib import admin
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from tasks.views import TaskViewSet
+# from .views import index
 
-router = DefaultRouter()
-router.register(r'tasks', TaskViewSet, basename='task')
+# router = DefaultRouter()
+# router.register(r'tasks', TaskViewSet, basename='task')
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('tasks.urls')),
+#     path('', index, name='home')
+# ]
+
+from django.http import HttpResponse
+from django.urls import path
+
+def test(request):
+    return HttpResponse("Hello World!")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls')),
-    path('', index, name='home')
+    path('', test),
 ]
