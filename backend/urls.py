3f2a5f34b59fb967e4b9b04c173a5ea12415878c
vickyg49 <vikkyganesh7929@gmail.com>
2025-08-25 17:04:40 +0530

    change in urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from tasks.views import TaskViewSet
 from .views import index
-from .views import EmberAppView
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
@@ -11,6 +10,5 @@ router.register(r'tasks', TaskViewSet, basename='task')
 urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),  # -> exposes /api/tasks/
-    re_path(r'^(?!api/).*$', EmberAppView.as_view(), name='ember-app')
+    path('api/', include('your_app.urls'))
 ]
