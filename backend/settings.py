7a9b927cefef79a733dcf96dc836a49da1f265c0
vickyg49 <vikkyganesh7929@gmail.com>
2025-08-26 17:23:39 +0530

    Commit for serving ember app

--- backend/settings.py
+++ backend/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "frontend_dist"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
